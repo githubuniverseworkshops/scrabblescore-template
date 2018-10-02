bb320f6d95ac81a023133765560422797f9a26dd
Steve Winton <stevewinton@gmail.com>
2018-10-02 18:11:46 -0500

    Use explicit path to $HOME

--- .github/actions/npm/publish/entrypoint.sh
+++ .github/actions/npm/publish/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [[ $NPM_AUTH_TOKEN ]]; then
-  echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > $HOME/.npmrc
+  echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > /home/github/.npmrc
 fi
 
 exec npm "$@"
