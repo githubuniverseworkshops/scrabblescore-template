72d43400516785848a0b3f11d496403397466439
Steve Winton <stevewinton@gmail.com>
2018-10-02 18:33:04 -0500

    Bump version, set NPM_CONFIG_USERCONFIG

--- .github/actions/npm/publish/entrypoint.sh
+++ .github/actions/npm/publish/entrypoint.sh
@@ -2,6 +2,8 @@
 
 if [[ $NPM_AUTH_TOKEN ]]; then
   echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > $HOME/.npmrc
+  export NPM_CONFIG_USERCONFIG=$HOME/.npmrc
+  echo $NPM_CONFIG_USERCONFIG
 fi
 
 exec npm "$@"
